b0963635ee0c1110f73700bd87c0c721ee444b60
adrian-brady <mail@adrianbrady.com>
2024-08-21 15:18:54 -0500

    feat: add VM RETURN and CONSTANT interpretation

--- src/vm.c
+++ src/vm.c
@@ -1,4 +1,8 @@
+#include <stdio.h>
+
+#include "chunk.h"
 #include "common.h"
+#include "value.h"
 #include "vm.h"
 
 VM vm;
@@ -6,3 +10,32 @@ VM vm;
 void initVM() {}
 
 void freeVM() {}
+
+
+static InterpretResult run() {
+  #define READ_BYTE() (*vm.ip++)
+  #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+  for (;;) {
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+      case OP_CONSTANT: {
+        Value constant;
+        constant = READ_CONSTANT();
+        printValue(constant);
+        printf("\n");
+        break;
+      }
+      case OP_RETURN:
+        return INTERPRET_OK;
+    }
+  }
+  #undef READ_CONSTANT
+  #undef READ_BYTE
+}
+
+InterpretResult interpret(Chunk* chunk) {
+  vm.chunk = chunk;
+  vm.ip = vm.chunk->code;
+  return run();
+}
