1a517ee99532cac59e25774569041018a6d3b4fc
adrian-brady <mail@adrianbrady.com>
2024-08-21 16:23:28 -0500

    feat: add stack trace

--- src/vm.c
+++ src/vm.c
@@ -35,6 +35,13 @@ static InterpretResult run() {
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
+    printf("          ");
+    for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+      printf("[");
+      printValue(*slot);
+      printf("]");
+    }
+    printf("\n");
     disassembleInstruction(vm.chunk,
                            (int)(vm.ip - vm.chunk->code));
 #endif
